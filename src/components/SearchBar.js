ae36b167aa901ae4dc1c60fe990c6feb7390691a
Moraru Ion <morarou.y@gmail.com>
2017-10-01 17:14:51 +0200

    add inverse dataflow

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+        this.handleInStockChange = this.handleInStockChange.bind(this);
+    }
+
+    handleFilterTextChange(e) {
+        this.props.onFilterTextChange(e.target.value);
+    }
+
+    handleInStockChange(e) {
+        this.props.onInStockChange(e.target.checked);
+    }
+
     render() {
         const { filterText, inStockOnly } = this.props;
         
@@ -9,12 +24,14 @@ class SearchBar extends Component {
                 <input 
                     type="text" 
                     placeholder="Search..."
+                    onChange={this.handleFilterTextChange}
                     value={filterText} 
                 />
                 <br />
                 <input 
                     type="checkbox" 
                     checked={inStockOnly}
+                    onChange={this.handleInStockChange}
                 />
                 {' '}
                 Only show products in stock
